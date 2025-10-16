ff4f93cfb6172edc2a85f243fdf7d2e0c1c132a7
ydbr0918 <inseo060918@g.shingu.ac.kr>
2025-10-16 14:04:24 +0900

    [Feat] 카메라 시점고정

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; 
+    public Vector3 offset = new Vector3(0, 10, -10); 
+    public float smoothSpeed = 5f; 
+
+    void LateUpdate()
+    {
+        if (target == null) return;
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+        transform.LookAt(target);
+    }
+}
